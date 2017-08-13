91e5d6b3db5ba84acb4c955d8dbf58b509b0793c
frankbozar <frankbozar@frank.local>
2017-08-13 23:23:33 +0800

    Z

--- code/z.cpp
+++ code/z.cpp
@@ -1,15 +1,9 @@
-void z_algo(char *s, int z[]){
-	z[0]=strlen(s);
-	for(int n=strlen(s), l=0, r=0, i=1; i<n; i++){
-		if( i>r ){
-			for(l=r=i; r<n && s[r-l]==s[r]; r++);
-			z[i]=r---l;
-		}else{
-			if( z[i-l]<r-i+1 ) z[i]=z[i-l];
-			else{
-				for(l=i; R<n && s[R-L]==s[R]; R++);
-                z[i]=r---l;
-			}
-		}
-	}
+void Z(const char* s, int z[]){
+    int n=z[0]=strlen(s);
+	for(int l=0, r=0, i=1; i<n; i++)
+        if( r<i || r-i<z[i-l] ){
+            if( r<i ) r=i;
+            for(l=i; r<n && s[r]==s[r-l]; r++);
+            z[i]=r---l;
+        }else z[i]=z[i-l];
 }
