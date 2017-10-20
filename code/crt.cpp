978edfb0cea0d064fe4da806b0de76194a30e2ed
frankbozar <frankbozar@frank.local>
2017-10-20 18:51:41 +0800

    formatting

--- code/crt.cpp
+++ code/crt.cpp
@@ -1,7 +1,6 @@
 lld CRT(int n, lld a[], lld m[]){
 	for(int i=1; i<n; i++){
-		lld x, y, g=ext_gcd(m[i-1], m[i], x, y);
-		lld r=a[i]-a[i-1];
+		lld x, y, g=ext_gcd(m[i-1], m[i], x, y), r=a[i]-a[i-1];
 		if( r%g!=0 ) return -1;
 		lld t=m[i]/g;
 		x=(r/g*x%t+t)%t;
--- code/mod_eq.cpp
+++ code/mod_eq.cpp
@@ -4,8 +4,7 @@ vector<lld> mod_eq(lld a, lld b, lld n){
 	if( b%g==0 ){
 		x=((x%n)+n)%n;
 		ans.push_back(x*(b/g)%(n/g));
-		for(lld i=1; i<g; i++)
-			ans.push_back((ans[0]+i*n/g)%n);
+		for(lld i=1; i<g; i++) ans.push_back((ans[0]+i*n/g)%n);
 	}
 	return ans;
 }
