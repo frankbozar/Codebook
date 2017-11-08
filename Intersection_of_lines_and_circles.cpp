13c55c791271eea492bec4032c554c80ad154d34
frankbozar <frankbozar@frank.local>
2017-11-08 22:46:10 +0800

    file cnt

--- Intersection_of_lines_and_circles.cpp
+++ Intersection_of_lines_and_circles.cpp
@@ -9,3 +9,14 @@ vector<pdd> interCircle(pdd o1, double r1, pdd o2, double r2) {
 	pdd v = A / (2*d2) * pdd(o1.S-o2.S, -o1.F+o2.F);
 	return {u+v, u-v};
 }
+//
+pdd interPnt(pdd p1, pdd p2, pdd q1, pdd q2, bool &res){
+    double f1 = cross(p2, q1, p1);
+    double f2 = -cross(p2, q2, p1);
+    double f = (f1 + f2);
+    if(fabs(f) < EPS) {
+        res = false; return {};
+    }
+    res = true;
+    return (f2 / f) * q1 + (f1 / f) * q2;
+}
