91ab2dc07a5797224c446b57ac669a3f89f0d4a1
frankbozar <frankbozar@frank.local>
2017-11-08 23:23:04 +0800

    geometry

---
+++ CircleCover.cpp
@@ -0,0 +1,77 @@
+#define N 1021
+struct CircleCover{
+	int C; Circle c[ N ];
+	bool g[ N ][ N ], overlap[ N ][ N ];
+	// Area[i] : area covered by at least i circles
+	D Area[ N ];
+	void init( int _C ){ C = _C; }
+	bool CCinter( Circle& a , Circle& b , Pt& p1 , Pt& p2 ){
+		Pt o1 = a.O , o2 = b.O;
+		D r1 = a.R , r2 = b.R;
+		D d2 = ( o1 - o2 ) * ( o1 - o2 );
+		D d = sqrt(d2);
+		if( d > r1 + r2 ) return false;
+		Pt u = (o1+o2)*0.5 + (o1-o2)*((r2*r2-r1*r1)/(2*d2));
+		D A = sqrt((r1+r2+d)*(r1-r2+d)*(r1+r2-d)*(-r1+r2+d));
+		Pt v = Pt( o1.Y-o2.Y , -o1.X + o2.X ) * A / (2*d2);
+		p1 = u + v; p2 = u - v;
+		return true;
+	}
+	struct Tevent {
+		Pt p; D ang; int add;
+		Tevent() {}
+		Tevent(Pt _a, D _b, int _c): p(_a), ang(_b), add(_c) {}
+		bool operator<(const Tevent &a)const
+		{return ang < a.ang;}
+	}eve[ N * 2 ];
+	// strict: x = 0, otherwise x = -1
+	bool disjuct( Circle& a, Circle &b, int x ){
+		return sign( norm( a.O - b.O ) - a.R - b.R ) > x;
+	}
+	bool contain( Circle& a, Circle &b, int x ){
+		return sign( a.R - b.R - norm( a.O - b.O ) ) > x;
+	}
+	bool contain(int i, int j){ /* c[j] is non-strictly in c[i]. */
+		return (sign(c[i].R - c[j].R) > 0 ||
+				(sign(c[i].R - c[j].R) == 0 && i < j) ) && contain(c[i], c[j], -1);
+	}
+	void solve(){
+		for( int i = 0 ; i <= C + 1 ; i ++ )
+			Area[ i ] = 0;
+		for( int i = 0 ; i < C ; i ++ )
+			for( int j = 0 ; j < C ; j ++ )
+				overlap[i][j] = contain(i, j);
+		for( int i = 0 ; i < C ; i ++ )
+			for( int j = 0 ; j < C ; j ++ ) 
+				g[i][j] = !(overlap[i][j] || overlap[j][i] ||
+						disjuct(c[i], c[j], -1));
+		for( int i = 0 ; i < C ; i ++ ){
+			int E = 0, cnt = 1;
+			for( int j = 0 ; j < C ; j ++ )
+				if( j != i && overlap[j][i] )
+					cnt ++;
+			for( int j = 0 ; j < C ; j ++ )
+				if( i != j && g[i][j] ){
+					Pt aa, bb;
+					CCinter(c[i], c[j], aa, bb);
+					D A = atan2(aa.Y - c[i].O.Y, aa.X - c[i].O.X);
+					D B = atan2(bb.Y - c[i].O.Y, bb.X - c[i].O.X);
+					eve[E ++] = Tevent(bb, B, 1);
+					eve[E ++] = Tevent(aa, A, -1);
+					if(B > A) cnt ++;
+				}
+			if( E == 0 ) Area[ cnt ] += pi * c[i].R * c[i].R;
+			else{
+				sort( eve , eve + E );
+				eve[E] = eve[0];
+				for( int j = 0 ; j < E ; j ++ ){
+					cnt += eve[j].add; 
+					Area[cnt] += (eve[j].p ^ eve[j + 1].p) * .5;
+					D theta = eve[j + 1].ang - eve[j].ang;
+					if (theta < 0) theta += 2. * pi;
+					Area[cnt] += ( theta - sin(theta) ) * c[i].R * c[i].R * .5;
+				}
+			}
+		}
+	}
+};
