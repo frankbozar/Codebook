44764017db76e9512a55d798cdcde7d05145e952
frankbozar <frankbozar@frank.local>
2017-11-08 22:43:50 +0800

    nut

---
+++ pdd.cpp
@@ -0,0 +1,26 @@
+#define x first
+#define y second
+#define cpdd const pdd
+struct pdd : pair<double, double> {
+	using pair<double, double>::pair;
+	pdd operator + (cpdd &p) const {
+		return {x+p.x, y+p.y};
+	}
+	pdd operator - () const {
+		return {-x, -y};
+	}
+	pdd operator - (cpdd &p) const {
+		return (*this) + (-p);
+	}
+	pdd operator * (double f) const {
+		return {f*x, f*y};
+	}
+	double operator * (cpdd &p) const {
+		return x*p.x + y*p.y;
+	}
+};
+double abs(cpdd &p) { return hypot(p.x, p.y); }
+double arg(cpdd &p) { return atan2(p.y, p.x); }
+double cross(cpdd &p, cpdd &q) { return p.x*q.y - p.y*q.x; }
+double cross(cpdd &p, cpdd &q, cpdd &o) { return cross(p-o, q-o); }
+pdd operator * (double f, cpdd &p) { return p*f; } // !! Not f*p !!
